0fb3e99cb837ed858d28129c86d4093f422f4704
elzar314 <114011666+elzar314@users.noreply.github.com>
2023-06-30 17:24:44 -0500

    Create e_tema1.java

---
+++ java_basico/e_tema1.java
@@ -0,0 +1,25 @@
+public class Main {
+  public static void main(String[] args) {
+  	
+    //Creando y asignando variables
+    
+   	int variable1 = 12;
+    long variable2 = 156465487;
+    char variable3 = 'a';
+    String variable4 = "Hola mundo";
+    boolean variable5 = true;
+    float variable6 = 6.654F;
+    double variable7 = 7.8989865D;
+    
+    //Imprimiento variables
+    
+    System.out.println("Ejemplos de variables en Java\n");
+    System.out.println("int: "+variable1);
+    System.out.println("long: "+variable2);
+    System.out.println("char: "+variable3);
+    System.out.println("String: "+variable4);
+    System.out.println("boolean: "+variable5);
+    System.out.println("float: "+variable6);
+    System.out.println("double: "+variable7);
+  }
+}
