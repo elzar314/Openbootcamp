39ef9221d31ad96439fe214f9c29c5dd2528a516
elzar314 <114011666+elzar314@users.noreply.github.com>
2023-07-02 16:19:11 -0500

    Create ejercicio1.java

---
+++ java_basico/eFinales/ejercicio1.java
@@ -0,0 +1,11 @@
+package eFinales;
+
+public class ejercicio1 {
+    public static void main(String[] args) {
+        String [] paises = {"Australia","Perú","Mexico","España","USA","Alemania","Bélgica"};
+
+        for(String e:paises){
+            System.out.print(e+" ");
+        }
+    }
+}
