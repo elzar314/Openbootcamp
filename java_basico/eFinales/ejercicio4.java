3d27dd9820bc05704253c03489b92c979faa73fd
elzar314 <114011666+elzar314@users.noreply.github.com>
2023-07-02 16:56:58 -0500

    Create ejercicio4.java

---
+++ java_basico/eFinales/ejercicio4.java
@@ -0,0 +1,23 @@
+package eFinales;
+
+import java.util.Vector;
+
+public class ejercicio4 {
+    public static void main(String[] args) {
+      
+        System.out.println("El problema de utilizar un Vector con la capacidad por defecto al tener 1000 " +
+                "elementos para ser añadidos es la necesidad de incrementar y copiar automáticamente los " +
+                "elementos en múltiples realocaciones, lo cual puede llevar a un consumo de memoria innecesario" +
+                " y a una degradación del rendimiento. Se recomienda proporcionar una capacidad inicial adecuada " +
+                "al crear el Vector para evitar estos problemas.");
+
+        Vector<Integer> numeros = new Vector(1000);
+
+        for(int i=0;i<1000;i++){
+            numeros.add(i+1);
+        }
+
+        System.out.println(numeros);
+
+    }
+}
