7f9b6ee5b1cb964f77213539be2a5208f12de3aa
elzar314 <114011666+elzar314@users.noreply.github.com>
2023-07-03 02:41:55 -0500

    Update ejercicio7.java

--- java_basico/eFinales/ejercicio7.java
+++ java_basico/eFinales/ejercicio7.java
@@ -1,4 +1,10 @@
 package eFinales;
+/*
+Crea una función DividePorCero. Esta, debe generar una excepción ("throws") a su llamante 
+    del tipo ArithmeticException que será capturada por su llamante (desde "main", por ejemplo). 
+    Si se dispara la excepción, mostraremos el mensaje "Esto no puede hacerse". 
+    Finalmente, mostraremos en cualquier caso: "Demo de código".
+*/
 
 public class ejercicio7 {
     public static void main(String[] args) {
