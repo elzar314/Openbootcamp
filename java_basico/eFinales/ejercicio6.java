f5ee258efe555f88fc044283b4b1ddc2fdac8da3
elzar314 <114011666+elzar314@users.noreply.github.com>
2023-07-03 02:41:08 -0500

    Update ejercicio6.java

--- java_basico/eFinales/ejercicio6.java
+++ java_basico/eFinales/ejercicio6.java
@@ -1,5 +1,10 @@
 package eFinales;
-
+/*
+Crea un ArrayList de tipo int, y, utilizando un bucle rellénalo con elementos 1..10.
+A continuación, con otro bucle, recórrelo y elimina los numeros pares. 
+Por último, vuelve a recorrerlo y muestra el ArrayList final. Si te atreves, 
+puedes hacerlo en menos pasos, siempre y cuando cumplas el primer "for" de relleno.
+*/
 import java.util.ArrayList;
 
 public class ejercicio6 {
